3c17d9ef0c6b7e3ce92e4f5dc42d81108b679800
Volkov Slava <volkov.frontend@gmail.com>
2024-04-29 15:38:18 +0300

    feat: add task_1 solution

--- task_1/index.js
+++ task_1/index.js
@@ -3,6 +3,34 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+function decoded(encoded, translations) {
+  const excludedKeys = ["groupId", "service", "formatSize", "ca"];
+  const uniqueIds = new Set();
 
+  const decodedData = encoded.map(obj => {
+    const decodedObj = {};
 
-// console.log(decoded)
+    for (const key in obj) {
+      if (key.endsWith("Id") && !excludedKeys.includes(key)) {
+        const decodedValue = translations[obj[key]];
+        if (decodedValue) {
+          decodedObj[key] = decodedValue;
+        }
+      } else {
+        decodedObj[key] = obj[key];
+      }
+
+      if (key.endsWith("Id")) {
+        uniqueIds.add(key);
+      }
+    }
+
+    return decodedObj;
+  });
+
+  return { decodedData, uniqueIds: Array.from(uniqueIds) };
+}
+
+const { decodedData, uniqueIds } = decoded(encoded, translations);
+console.log("Раскодированные данные:", decodedData);
+console.log("Уникальные id:", uniqueIds);
